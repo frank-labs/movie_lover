8a41f1da04b4451b5e69015eedf3bff29f090c26
frank-labs <wan9zhan9@gmail.com>
2024-12-01 06:56:11 -0500

    get postgress working, and initial view

--- movie_app/models.py
+++ movie_app/models.py
@@ -1,3 +1,56 @@
 from django.db import models
+import json
 
-# Create your models here.
+class Movie(models.Model):
+    id = models.BigIntegerField(primary_key=True)  # ID from the API, not auto-incremented
+    url = models.URLField(max_length=400,blank=True)  # URL to the movie's page
+    imdb_code = models.CharField(max_length=255, blank=True, null=True)
+    title = models.CharField(max_length=255, blank=True, null=True)
+    title_english = models.CharField(max_length=255, blank=True, null=True)
+    title_long = models.CharField(max_length=255, blank=True, null=True)
+    slug = models.SlugField(unique=True, max_length=255, blank=True, null=True)
+    year = models.IntegerField(blank=True, null=True)
+    rating = models.FloatField(blank=True, null=True)
+    runtime = models.IntegerField(blank=True, null=True)  # Runtime in minutes
+    genres = models.JSONField(blank=True, null=True)  # Store genres as JSON
+    summary = models.TextField(blank=True, null=True)  # Long text
+    description_full = models.TextField(blank=True, null=True)
+    synopsis = models.TextField(blank=True, null=True)
+    yt_trailer_code = models.CharField(max_length=255, blank=True, null=True)
+    language = models.CharField(max_length=255, blank=True, null=True)
+    mpa_rating = models.CharField(max_length=255, blank=True, null=True)
+    background_image = models.URLField(max_length=400, blank=True, null=True)
+    background_image_original = models.URLField(max_length=400,blank=True, null=True)
+    small_cover_image = models.URLField(max_length=400,blank=True, null=True)
+    medium_cover_image = models.URLField(max_length=400,blank=True, null=True)
+    large_cover_image = models.URLField(max_length=400,blank=True, null=True)
+    state = models.CharField(max_length=255, blank=True, null=True, default='ok')
+    torrents = models.JSONField(blank=True, null=True)  # Store torrents as JSON
+    date_uploaded = models.DateTimeField(blank=True, null=True)  # Date uploaded
+    date_uploaded_unix = models.BigIntegerField(blank=True, null=True)  # Unix timestamp for upload time
+
+    class Meta:
+        db_table = 'movies'  # Specifies that this model maps to the 'movies' table
+        indexes = [
+        models.Index(fields=['slug']),
+        models.Index(fields=['imdb_code']),
+        models.Index(fields=['title']),
+        models.Index(fields=['title_english']), 
+        models.Index(fields=['title_long']),
+        models.Index(fields=['year']),
+        models.Index(fields=['runtime']),
+        models.Index(fields=['rating']),
+        models.Index(fields=['date_uploaded']),
+        models.Index(fields=['date_uploaded_unix']),
+        models.Index(fields=['language']),
+        ]
+
+
+
+    def __str__(self):
+        return self.title if self.title else "Unnamed Movie"
+
+# class MovieAdmin(admin.ModelAdmin):
+#     search_fields = ['title', 'imdb_code', 'slug']
+#     list_filter = ['year', 'rating']
+#     list_display = ['title', 'year', 'rating']
--- movie_lover/settings.py
+++ movie_lover/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'movie_app.apps.MovieAppConfig',
+    # 'grappelli',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -74,9 +76,12 @@ WSGI_APPLICATION = 'movie_lover.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "OPTIONS": {
+            "service": "my_service",
+            "passfile": ".my_pgpass",
+        },
     }
 }
 
@@ -105,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Toronto'
 
 USE_I18N = True
 
