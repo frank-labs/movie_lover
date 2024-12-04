dfe6617731718e9f6215cc9a79550399e1a2a5c7
frank-labs <wan9zhan9@gmail.com>
2024-12-03 22:57:02 -0500

    collection, and watch later ajax url

--- movie_app/urls.py
+++ movie_app/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
+from .views import CollectionCreateView, CollectionListView, CollectionDetailView, toggle_watch_later
 
 from . import views
 # app_name = 'movie_app' 
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
     path("<int:pk>/", views.DetailView.as_view(), name="detail"),
-    path('login/', views.login_view, name='login'),
-    path('register/', views.register, name='register'),
-    path('logout/', views.logout_view, name='logout'),
+    path('create/', CollectionCreateView.as_view(), name='create_collection'),
+    path('collections/', CollectionListView.as_view(), name='collection_list'),  # List collections
+    path('collections/<int:pk>/', CollectionDetailView.as_view(), name='collection_detail'),  
+    path('toggle-watch-later/<int:movie_id>/', toggle_watch_later, name='toggle_watch_later'),
 ]
