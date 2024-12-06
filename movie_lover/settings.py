61f5f5622ada0793843a71f4280afc312566e448
frank-labs <wan9zhan9@gmail.com>
2024-12-06 12:59:12 -0500

    add ALLOWED_HOSTS for deploy

--- movie_lover/settings.py
+++ movie_lover/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-mwil(r3u3couvm+)l*y)oa+d@+_elim#zan9t%va@y7ugc(#%u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 SOCIALACCOUNT_AUTO_SIGNUP = False
-ALLOWED_HOSTS = ['localhost','192.168.0.194','0.0.0.0','127.0.0.1']
+ALLOWED_HOSTS = ['localhost','192.168.0.194','0.0.0.0','127.0.0.1','172.245.30.226','optiontruth.com','movielover.optiontruth.com']
 
 
 # Application definition
