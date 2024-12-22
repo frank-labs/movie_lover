2f969d585979f5b7473dbd7b0bccae12df0ffcbf
frank-labs <wan9zhan9@gmail.com>
2024-12-22 14:57:01 -0500

    add github

--- movie_lover/settings.py
+++ movie_lover/settings.py
@@ -74,6 +74,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'allauth.socialaccount.providers.google',
+    'allauth.socialaccount.providers.github',
 ]
 
 MIDDLEWARE = [
